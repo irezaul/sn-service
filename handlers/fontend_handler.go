8d1d06baa551ccc2ebd6fbccae75c1a638356fb1
raihaninfo <raihanmahmudi35@gmail.com>
2022-07-06 14:49:56 +0600

    create add services frontend

--- handlers/fontend_handler.go
+++ handlers/fontend_handler.go
@@ -1,11 +1,29 @@
 package handlers
 
 import (
+	"encoding/json"
+	"ginorm/model"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func FrontendService(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.gohtml", nil)
+	req, err := http.Get("http://localhost:8080/api/service")
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var service []model.Service
+	json.Unmarshal(body, &service)
+
+	c.HTML(http.StatusOK, "index.gohtml", service)
 }
